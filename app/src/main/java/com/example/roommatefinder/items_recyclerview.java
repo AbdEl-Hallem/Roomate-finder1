e72299a16d48ef8ac57131e2b9c5d7bd2105dc0b
AbdEl-Hallem <112423306+AbdEl-Hallem@users.noreply.github.com>
2023-05-18 17:18:33 +0200

    home page

---
+++ app/src/main/java/com/example/roommatefinder/items_recyclerview.java
@@ -0,0 +1,14 @@
+package com.example.roommatefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class items_recyclerview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_recyclerview);
+    }
+}
